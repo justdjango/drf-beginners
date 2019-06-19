1b20332d9d7111133adf2e8f9de90033459c16e1
Matthew Freire <matthewfreire@Matthews-iMac.local>
2019-06-19 20:38:51 +0200

    Overview

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostView(APIView):
+    permission_classes = (AllowAny, )
+
+    def get(self, request, *args, **kwargs):
+        queryset = Post.objects.all()
+        serializer = PostSerializer(queryset, many=True)
+        # if serializer.is_valid():
+        return Response(serializer.data)
