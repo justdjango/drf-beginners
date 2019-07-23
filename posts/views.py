cf0caa0c4265fc168a7c8ce873d772ffa7862996
Matthew Freire <matthewfreire@Matthews-iMac.local>
2019-07-23 21:52:48 +0200

    Routers and viewsets

--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework import viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT
@@ -128,3 +129,11 @@ class OwnerDetailView(generics.RetrieveAPIView):
 class CommentDetailView(generics.RetrieveAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (
+        AllowAny,
+    )
